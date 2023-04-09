940f5995221b3c4c87edd2b631b1bfdef1b25ec0
james <jamesliwood@gmail.com>
2023-04-09 00:59:39 -0400

    added grid.js

---
+++ src/components/grid.js
@@ -0,0 +1,36 @@
+import "../stylesheet/grid.css";
+import { colRef } from "../database/src/db";
+import { useState, useEffect } from "react";
+import { onSnapshot } from "../database/node_modules/firebase/firestore";
+import { Link } from "react-router-dom";
+
+const Grid = () => {
+  const [arr, setArr] = useState([]);
+
+  useEffect(() => {
+    onSnapshot(colRef, (snapshot) => {
+      setArr(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+
+  return (
+    <div className="grid">
+      {arr.map((doc) => {
+        return (
+          <div className="game-card" key={doc.Title}>
+            <Link to="/gameInfo" state={doc}>
+              <img
+                src={require(`../images/${doc.ImgLink}`)}
+                className="game-image"
+                alt={doc.Title}
+              />
+            </Link>
+            <div className="game-title">{doc.Title}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Grid;
