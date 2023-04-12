127ef2517999e52a3ef403eff2b30c1459c1822c
james <jamesliwood@gmail.com>
2023-04-12 00:22:40 -0400

    properly added sign-out button

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,22 @@ import "../stylesheet/sidebar.css";
 import { Link, useNavigate } from "react-router-dom";
 import { user } from "../pages/login";
 
+function activateLogOut() {
+  const active = document.getElementById("user").classList.toggle("active");
+  let logOut = document.getElementById("logOut");
+
+  if (active) {
+    logOut.style.opacity = 1;
+    logOut.style.pointerEvents = "visible";
+  } else {
+    logOut.style.opacity = 0;
+    logOut.style.pointerEvents = "none";
+  }
+}
+
 const Header = ({ expand }) => {
   const navigate = useNavigate();
-  function signOut() {
+  function logOut() {
     user.loggedIn = false;
     user.username = "";
     user.data = null;
@@ -35,8 +48,13 @@ const Header = ({ expand }) => {
         </div>
         <div>
           {user.loggedIn ? (
-            <div className="user" onClick={signOut}>
-              {user.username}
+            <div className="user-container">
+              <div className="user" id="user" onClick={activateLogOut}>
+                {user.username}
+              </div>
+              <div className="logOut" id="logOut" onClick={logOut}>
+                Log out
+              </div>
             </div>
           ) : (
             <Link to="/login">
