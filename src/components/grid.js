ad13b6d4866c5a1945f2f91827cbe5c2f9c73fe6
james <jamesliwood@gmail.com>
2023-04-17 02:45:34 -0400

    added conditionals in return statement

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,10 +1,9 @@
 import "../stylesheet/grid.css";
-import { colRef } from "../database/src/db";
-import { useState, useEffect } from "react";
 import { onSnapshot } from "../database/node_modules/firebase/firestore";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const Grid = () => {
+const Grid = ({ colRef }) => {
   const [arr, setArr] = useState([]);
 
   useEffect(() => {
@@ -14,22 +13,28 @@ const Grid = () => {
   }, []);
 
   return (
-    <div className="grid">
-      {arr.map((doc) => {
-        return (
-          <div className="game-card" key={doc.title}>
-            <Link to="/gameInfo" state={doc}>
-              <img
-                src={require(`../images/${doc.imgLink}`)}
-                className="game-image"
-                alt={doc.Title}
-              />
-            </Link>
-            <div className="game-title">{doc.title}</div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {arr.length > 0 ? (
+        <div className="grid">
+          {arr.map((doc) => {
+            return (
+              <div className="game-card" key={doc.title}>
+                <Link to="/gameInfo" state={doc}>
+                  <img
+                    src={require(`../images/${doc.imgLink}`)}
+                    className="game-image"
+                    alt={doc.Title}
+                  />
+                </Link>
+                <div className="game-title">{doc.title}</div>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="no-result">No Result</div>
+      )}
+    </>
   );
 };
 
