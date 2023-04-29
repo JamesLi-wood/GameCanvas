fe4f298effef580457a42df238f81e7cf55a23f0
james <jamesliwood@gmail.com>
2023-04-29 01:20:24 -0400

    added user deletion

--- src/components/changeCreds.js
+++ src/components/changeCreds.js
@@ -5,20 +5,32 @@ import {
   where,
   getDocs,
   updateDoc,
+  deleteDoc,
 } from "../database/node_modules/firebase/firestore";
 import { userRef } from "../database/src/db";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { setUser } from "../pages/App";
 import { useNavigate } from "react-router-dom";
 
 const ChangeCreds = ({ option, user }) => {
+  const [erase, setErase] = useState(false);
+  const [modify, setModify] = useState(false);
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(false);
   const [change, setChange] = useState();
   const navigate = useNavigate();
+  const docRef = doc(userRef, user);
+
+  useEffect(() => {
+    if (option == "delete") {
+      setErase(true);
+    } else if (option == "username" || "password") {
+      setModify(true);
+    }
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const docRef = doc(userRef, user);
 
     if (option === "username") {
       const q = query(userRef, where("username", "==", change));
@@ -43,22 +55,39 @@ const ChangeCreds = ({ option, user }) => {
     }
   };
 
+  function deleteAccount() {
+    deleteDoc(docRef).then(() => {
+      setUser(false, "", null);
+      navigate("/home");
+    });
+  }
+
   return (
-    <div className="test">
-      <div>Please enter new {option}:</div>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="input"
-          type="text"
-          placeholder="Username"
-          onChange={(e) => setChange(e.target.value)}
-          required
-        />
-        <button>Submit</button>
-      </form>
-      {error && <div className="error">Username is already taken.</div>}
-      {success && <div>Password successfully changed!</div>}
-    </div>
+    <>
+      {modify && (
+        <div className="change-details">
+          <div>Please enter new {option}:</div>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="input"
+              type="text"
+              placeholder="Username"
+              onChange={(e) => setChange(e.target.value)}
+              required
+            />
+            <button>Submit</button>
+          </form>
+          {error && <div className="error">Username is already taken.</div>}
+          {success && <div>Password successfully changed!</div>}
+        </div>
+      )}
+      {erase && (
+        <div className="change-details">
+          <div>Are you sure you want to delete?</div>
+          <button onClick={deleteAccount}>Delete</button>
+        </div>
+      )}
+    </>
   );
 };
 
