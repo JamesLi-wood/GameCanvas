37fcb7b451c0653551fec5b7fa2450182c6889bb
james <jamesliwood@gmail.com>
2023-05-01 03:52:28 -0400

    updated profile list

--- src/pages/profileList.js
+++ src/pages/profileList.js
@@ -1,31 +1,60 @@
 import {
-  where,
-  query,
-  getDocs,
+  getDoc,
   onSnapshot,
+  doc,
 } from "../database/node_modules/firebase/firestore";
+import { db, colRef } from "../database/src/db";
 import { useState, useEffect } from "react";
-import { userRef } from "../database/src/db";
+import { Link } from "react-router-dom";
 import Header from "../components/header";
+import "../stylesheet/profile.css";
+import "../stylesheet/grid.css";
 
 const ProfileList = () => {
-  const [likes, setLikes] = useState([]);
+  const user = JSON.parse(sessionStorage.getItem("user"));
+  const docRef = doc(db, "Users", user.data);
+  const [arr, setArr] = useState([]);
+
   useEffect(() => {
-    onSnapshot(userRef, (snapshot) => {
-      snapshot.docs.map((doc) => {
-        setLikes(doc.get("likes"));
+    getDoc(docRef).then((data) => {
+      onSnapshot(colRef, (snapshot) => {
+        setArr(
+          snapshot.docs.map((doc) => {
+            if (data.data().likes.includes(doc.id)) {
+              return doc.data();
+            }
+          })
+        );
       });
     });
   }, []);
-  console.log(likes);
+  const likes = arr.filter((game) => game != undefined);
 
   return (
-    <div className="app-body">
-      <Header />
-      {likes.map((doc) => {
-        return <div key={doc}>{doc}</div>;
-      })}
-    </div>
+    <>
+      <div className="profile-body">
+        <Header />
+        <div className="favorite-card">
+          <div className="favorite">Your Favorites</div>
+          <div className="grid">
+            {likes.map((doc) => {
+              return (
+                <div className="game-card" key={doc.title}>
+                  <Link to="/gameInfo" state={doc}>
+                    <img
+                      src={require(`../images/${doc.imgLink}`)}
+                      className="game-image"
+                      alt={doc.Title}
+                    />
+                  </Link>
+                  <div className="game-title">{doc.title}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
