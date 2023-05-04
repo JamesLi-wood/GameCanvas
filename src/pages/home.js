c4768fb680f0f29af29cb31cc64dda2be9593ca8
james <jamesliwood@gmail.com>
2023-05-04 03:17:47 -0400

    limited the homepage to display a maximum of 24 entries

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,13 +2,16 @@ import "../stylesheet/home.css";
 import Header from "../components/header.js";
 import Grid from "../components/grid.js";
 import { colRef } from "../database/src/db";
+import { query, limit } from "../database/node_modules/firebase/firestore";
 
 function Home() {
+  const q = query(colRef, limit(24));
+
   return (
     <div className="center-body">
       <Header />
       <div className="home-card">
-        <Grid colRef={colRef} />
+        <Grid colRef={q} />
       </div>
     </div>
   );
