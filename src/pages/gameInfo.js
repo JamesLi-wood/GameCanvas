f6afebdb498f0b6f5cf3b10c7ed5c4ee28657226
james <jamesliwood@gmail.com>
2023-05-04 03:17:00 -0400

    added a current count to display how many users has favorited a certain game

--- src/pages/gameInfo.js
+++ src/pages/gameInfo.js
@@ -1,7 +1,7 @@
 import "../stylesheet/gameInfo.css";
 import "../stylesheet/home.css";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   getDoc,
   updateDoc,
@@ -11,19 +11,29 @@ import { db } from "../database/src/db";
 import Header from "../components/header.js";
 
 const GameInfo = () => {
-  const [isFavorite, setIsFavorite] = useState(false);
   const user = JSON.parse(sessionStorage.getItem("user"));
   const navigate = useNavigate();
   const location = useLocation();
-  const game = location.state;
+  const title = location.state;
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [game, setGame] = useState();
   let currentLikes;
   let docRef;
+
+  useEffect(() => {
+    setTimeout(() => {
+      const gameRef = doc(db, "Games", title);
+      getDoc(gameRef).then((doc) => {
+        setGame(doc.data());
+      });
+    }, 500);
+  }, []);
 
   if (user.loggedIn) {
     docRef = doc(db, "Users", user.data);
     getDoc(docRef).then((doc) => {
       currentLikes = doc.data().likes;
-      if (doc.data().likes.includes(game.title)) {
+      if (currentLikes.includes(title)) {
         setIsFavorite(true);
       } else {
         setIsFavorite(false);
@@ -37,14 +47,20 @@ const GameInfo = () => {
     } else {
       if (option == "check") {
         currentLikes.push(game.title);
+        game.favorites++;
       } else {
         currentLikes = currentLikes.filter((title) => title != game.title);
+        game.favorites--;
       }
 
       updateDoc(docRef, {
         likes: currentLikes,
       }).then(() => {
-        navigate(0);
+        const gameRef = doc(db, "Games", game.title);
+        updateDoc(gameRef, {
+          favorites: game.favorites,
+        });
+        navigate(0, { state: doc.title });
       });
     }
   }
@@ -52,57 +68,70 @@ const GameInfo = () => {
   return (
     <div className="center-body">
       <Header />
-      <div>
-        <div className="info-card">
-          <img
-            className="image"
-            src={require(`../images/${game.imgLink}`)}
-            alt={game.Title}
-          />
-          <div className="information">
-            <div className="title">{game.title}</div>
-            {isFavorite ? (
-              <div
-                id="star"
-                className="star-checked"
-                onClick={(event) => handleClick(event, "uncheck")}
-              >
-                &#9733;
+      {game ? (
+        <div>
+          <div className="info-card">
+            <img
+              className="image"
+              src={require(`../images/${game.imgLink}`)}
+              alt={game.title}
+            />
+            <div className="information">
+              <div className="title">{game.title}</div>
+
+              <div className="star-container">
+                <div>
+                  <div className="favorite-count">{game.favorites}</div>
+                  {isFavorite ? (
+                    <div
+                      id="star"
+                      className="star-checked"
+                      onClick={(event) => handleClick(event, "uncheck")}
+                    >
+                      &#9733;
+                    </div>
+                  ) : (
+                    <div
+                      id="star"
+                      className="star-unchecked"
+                      onClick={(event) => handleClick(event, "check")}
+                    >
+                      &#9734;
+                    </div>
+                  )}
+                </div>
               </div>
-            ) : (
-              <div
-                id="star"
-                className="star-unchecked"
-                onClick={(event) => handleClick(event, "check")}
-              >
-                &#9734;
+
+              <div className="data">Developer: {game.developer}</div>
+              <div className="data">Publisher: {game.publisher}</div>
+              <div className="data">Release Date: {game.releaseDate}</div>
+              <div className="data">Age rating: {game.rating}</div>
+              <div className="data">
+                Platforms:{" "}
+                {game.platforms.map((platform) => {
+                  return `${platform}, `;
+                })}
               </div>
-            )}
-            <div className="data">Developer: {game.developer}</div>
-            <div className="data">Publisher: {game.publisher}</div>
-            <div className="data">Release Date: {game.releaseDate}</div>
-            <div className="data">Age rating: {game.rating}</div>
-            <div className="data">
-              Platforms:{" "}
-              {game.platforms.map((platform) => {
-                return `${platform}, `;
-              })}
             </div>
           </div>
+          <div className="fill-card">
+            <div className="strong">Description:</div>
+            <div className="description">{game.description}</div>
+            <div className="strong">Genre:</div>
+            {game.genres.map((genre) => {
+              return (
+                <div className="genre-box" key={genre}>
+                  {genre}
+                </div>
+              );
+            })}
+          </div>
         </div>
-        <div className="fill-card">
-          <div className="strong">Description:</div>
-          <div className="description">{game.description}</div>
-          <div className="strong">Genre:</div>
-          {game.genres.map((genre) => {
-            return (
-              <div className="genre-box" key={genre}>
-                {genre}
-              </div>
-            );
-          })}
+      ) : (
+        <div className="home-card">
+          <div className="loading">Loading...</div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
