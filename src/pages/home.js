1661e81239e9ebb73c523f04303f1e1fa3cafe88
james <jamesliwood@gmail.com>
2023-05-09 03:23:53 -0400

    got rid of query and pass the gameRef as the prop

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,14 @@
 import "../stylesheet/home.css";
 import { gameRef } from "../database/src/db";
-import { query, limit } from "../database/node_modules/firebase/firestore";
 import Header from "../components/header.js";
 import Grid from "../components/grid.js";
 
 function Home() {
-  const q = query(gameRef, limit(24));
-
   return (
     <div className="center-body">
       <Header />
       <div className="home-card">
-        <Grid colRef={q} />
+        <Grid colRef={gameRef} />
       </div>
     </div>
   );
